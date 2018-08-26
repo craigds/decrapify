ee72b63287df93455e64131b12f17211b552c0ed
Craig de Stigter <craig.destigter@koordinates.com>
2018-08-26 21:22:05 +1200

    fstrings: Add support for rewriting .format()

--- fstrings.py
+++ fstrings.py
@@ -10,20 +10,31 @@ Implemented:
     * 'stringliteral %s %d' % (foo, bar)
         --> f'stringliteral {foo} {bar}'
 
+    * 'stringliteral {} {bar}'.format(foo, bar=bar)
+        --> f'stringliteral {foo} {bar}'
+
 TODO: plenty:
     * Handle other printf-style things other than %s/%d/%f, e.g. '%20d' etc.
-    * Handle .format(), with both args and kwargs
+    * Same for .format() - handle '!20d' etc
+    * Handle old-style dict interpolation, e.g. '%(a)s' % {'a': 'a'}
 """
 
 import argparse
 import re
 import sys
 
-from bowler import Query, TOKEN
-from bowler.types import Leaf, Node
+from bowler import Query, TOKEN, SYMBOL
+from bowler.types import Leaf, Node, STARS
+
+flags = {}
 
 
 RE_OLD_INTERPOLATION_BASIC = re.compile(r'(?<!%)%[fds]')
+
+# TODO: currently excludes modifiers like `{!d}`
+# TODO/FIXME: what's the syntax for a negative lookahead? Need to avoid matching '}}' escape.
+# No 're' docs on my flight :(
+RE_NEW_INTERPOLATION_BASIC = re.compile(r'(?<!{)\{[^{}!]*\}')
 
 
 def old_interpolation_to_fstrings(node, capture, filename):
@@ -31,8 +42,6 @@ def old_interpolation_to_fstrings(node, capture, filename):
     '%s' % xyz
         --> f'{xyz}'
     """
-    print("Selected expression: ", list(node.children))
-
     formatstring = capture['formatstring']
     interpolation_args = capture['interpolation_args']
     if isinstance(interpolation_args, Leaf):
@@ -62,6 +71,13 @@ def old_interpolation_to_fstrings(node, capture, filename):
         lambda matchobj: ('{%s}' % interpolation_args.pop(0)),
         formatstring.value,
     )
+
+    if flags['debug']:
+        print(f"Interpolating (old-style) format-string:\n\t{formatstring}")
+        print(f"With arguments:\n\t{interpolation_args}")
+        print(f"Replacement formatstring: {replacement_value}")
+        print()
+
     # Make sure we consumed all the arguments, otherwise something went wrong.
     assert not interpolation_args
 
@@ -73,6 +89,112 @@ def old_interpolation_to_fstrings(node, capture, filename):
     formatstring.value = replacement_value
     node.children[1:] = []
 
+    return node
+
+
+def _interpret_format_arguments(arg):
+    """
+    Recursive generator.
+
+    Given a single name, an argument, an arglist or a vararg expression,
+    returns actual argument values, in order.
+
+    Yields one of:
+        positional args: yields the arg value
+        keyword args: yields a dict {k: v}
+        other (complex nested structures or varargs): yields None.
+    )
+    """
+    if isinstance(arg, Leaf) and arg.type == TOKEN.COMMA:
+        # Skip comma tokens between actual args
+        return
+
+    if isinstance(arg, list):
+        # Handle top-level list of args. Also handles there being no args at all: .format()
+        for sub_arg in arg:
+            yield from _interpret_format_arguments(sub_arg)
+        return
+
+    if isinstance(arg, Node) and arg.type == SYMBOL.arglist:
+        # Multiple arguments, may be either keyword or positional
+        for child in arg.children:
+            yield from _interpret_format_arguments(child)
+        return
+
+    if isinstance(arg, Node) and arg.type == SYMBOL.argument:
+        if arg.children[0].type in STARS:
+            # *args, or **kwargs.
+            # Not useful for f-stringing. Give up.
+            yield None
+            return
+
+        # Single keyword argument: .format(keyword=value)
+        # The three child nodes here are (keyword, '=', value).
+        value = arg.children[2]
+        if not isinstance(value, Leaf):
+            # Might be complex? Give up. This stops parsing of the entire expression,
+            # beacuse having an f-string *and* a .format() is pretty nasty.
+            yield None
+        else:
+            yield {
+                arg.children[0].value: value.value
+            }
+    elif isinstance(arg, Leaf) and arg.type == TOKEN.NAME:
+        # Single positional argument, which is just a name.
+        yield arg.value
+    else:
+        # Something else.
+        # Might be a complex expression? Give up. This stops parsing of the entire expression,
+        # because having an f-string *and* a .format() is pretty nasty.
+        yield None
+
+
+def format_method_to_fstrings(node, capture, filename):
+    """
+    '{}'.format(xyz)
+        --> f'{xyz}'
+    """
+
+    if flags['debug']:
+        print("Selected expression: ", list(node.children))
+
+    formatstring = capture['formatstring']
+    interpolation_args = capture['interpolation_args']
+
+    # We only convert .format() stuff to an f-string if the arguments are all simple-ish.
+    # That means:
+    #  * name-only: `abc`
+
+    positional_args = []
+    keyword_args = {
+        # Maps kwarg names (strings) to the kwarg *value*
+        # e.g. for .format(a=b) this would be {'a': 'b'}
+    }
+    for parsed_arg in _interpret_format_arguments(interpolation_args):
+        if parsed_arg is None:
+            # This arg was deemed too complex to bother pushing into an f-string.
+            # Give up.
+            return node
+        elif isinstance(parsed_arg, dict):
+            keyword_args.update(parsed_arg)
+        else:
+            positional_args.append(parsed_arg)
+
+    # Actually push the new names into a new formatstring. Wrap each value with curly braces.
+    replacement = formatstring.value.format(
+        *['{%s}' % a for a in positional_args],
+        **{k: '{%s}' % v for (k, v) in keyword_args.items()}
+    )
+    if flags['debug']:
+        print(f"Interpolating (new-style) format-string:\n\t{formatstring}")
+        print(f"With arguments:\n\t{positional_args}, {keyword_args}")
+        print(f"Replacement formatstring: {replacement}")
+        print()
+
+    # Finally, apply the whole thing
+    formatstring.value = replacement
+    capture['trailer1'].remove()
+    capture['trailer2'].remove()
     return node
 
 
@@ -95,12 +217,24 @@ def main():
         help="Don't write the changes to the source file, just output a diff to stdout"
     )
     parser.add_argument(
+        '--debug',
+        dest='debug',
+        default=False,
+        action='store_true',
+        help="Spit out debugging information"
+    )
+    parser.add_argument(
         'files',
         nargs='+',
         help="The python source file(s) to operate on."
     )
     args = parser.parse_args()
 
+    # No way to pass this to .modify() callables, so we just set it at module level
+    flags['debug'] = args.debug
+
+    # TODO: merge these two queries, once I figure out how.
+    # https://github.com/facebookincubator/Bowler/issues/1
     query = (
         # Look for files in the current working directory
         Query(*args.files)
@@ -122,9 +256,32 @@ def main():
             )
         ''')
         .modify(callback=old_interpolation_to_fstrings)
+        .execute(
+            # interactive diff implies write (for the bits the user says 'y' to)
+            interactive=(args.interactive and args.write),
+            write=args.write,
+        )
+    )
+    (
+        Query(*args.files)
 
-        # 2. TODO: New-style interpolation (.format(...))
-
+        # 2. New-style interpolation (.format(...))
+        # The 'power<>' thing is confusing to me. What's 'power' mean in this context?
+        # NOTE: this selector is quite loose; it accepts 'any*' in the arguments to format().
+        # i.e. this happily accepts: ''.format(a, 2, b=3, c=d[e], *x, **y)
+        # We'll need to be careful handling each of these in the modify callback,
+        # since not all of those args make much sense shoved into an fstring.
+        .select('''
+            function_call=power<
+                formatstring=STRING
+                trailer1=trailer<
+                    '.' 'format'
+                >
+                trailer2=trailer< '(' interpolation_args=any* ')' >
+                any*
+            >
+        ''')
+        .modify(callback=format_method_to_fstrings)
 
         .execute(
             # interactive diff implies write (for the bits the user says 'y' to)
--- testfile.py
+++ testfile.py
@@ -10,10 +10,24 @@ c = 'this is a string'
 
 interpolated = 'interpolated'
 string = 'string'
+a_dict = {'a': 'interpolated'}
 
 'this is an %s string' % interpolated
 'this is an %s string' % (interpolated,)
 'this is an %s %s' % (interpolated, string)
 'this is an %s %s' % (interpolated, string,)
+
 'this is an {} string'.format(interpolated)
+'this is an {} {}'.format(interpolated, string)
+'this is an {1} {0}'.format(string, interpolated)
+
+'this is an {} {string}'.format(interpolated, string=string)
+'this is an {} {xyz}'.format(interpolated, xyz=string)
+'this is an {a} {0}'.format(string, **a_dict)
+
 'this is an {interpolated} string'.format(interpolated=interpolated)
+
+# Cases to *ignore* (mostly we only really want basic names in f-strings,
+# because including too much can harm readability)
+'this is an {} string'.format('interpolated')
+'this is an {} string'.format(a_dict['a'])
